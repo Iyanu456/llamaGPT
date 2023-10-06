d5693e78cab0a8a95dcf7224c1f4fce560d8984e
Iyanu456 <oyerindei13@gmail.com>
2023-10-07 00:36:44 +0100

    changing branch

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,11 @@
 import HankoProfile from "../components/HankoProfile"
+import { LogoutBtn } from "../components/LogoutButton"
 
 export default function Dashboard() {
     return (
         <>
             <HankoProfile />
+            <LogoutBtn />
         </>
     )
 }
