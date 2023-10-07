b220eaa1f153d96ae669a75485117245cf494457
Iyanu456 <oyerindei13@gmail.com>
2023-10-07 15:09:53 +0100

    deployment

---
+++ src/app/components/HankoProfile.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { useEffect } from "react";
+import { register } from "@teamhanko/hanko-elements";
+
+const hankoApi = `https://80aee7d4-d409-4b1a-8581-22e849ff9323.hanko.io`;
+
+export default function HankoProfile() {
+  useEffect(() => {
+    register(hankoApi).catch((error) => {
+      // handle error
+    });
+  }, []);
+
+return <hanko-profile />;
+}
+
---
+++ src/app/login/page.js
@@ -0,0 +1,10 @@
+import HankoAuth from "../components/HankoAuth";
+
+export default function LoginPage() {
+ return (
+    <div className="grid place-items-center min-h-[100vh]">
+        <HankoAuth />
+    </div>
+   
+ );
+}
