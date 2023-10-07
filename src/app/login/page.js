247e7eff048b16c8096d0e721b958a7260a37f76
Iyanu456 <oyerindei13@gmail.com>
2023-10-07 01:03:15 +0100

    new

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,6 +2,9 @@ import HankoAuth from "../components/HankoAuth";
 
 export default function LoginPage() {
  return (
-   <HankoAuth />
+    <div className="grid place-items-center min-h-[100vh]">
+        <HankoAuth />
+    </div>
+   
  );
 }
