45603241c7d79b1f23314c0234aa49013d49a6f8
Iyanu456 <oyerindei13@gmail.com>
2023-10-12 19:56:30 +0100

    api-test3

--- src/app/components/HankoImports.js
+++
@@ -1,8 +0,0 @@
-// hankoImports.js
-import dynamic from "next/dynamic";
-
-export const importHankoElements = () =>
-  dynamic(() => import("@teamhanko/hanko-elements"), { ssr: false });
-
-export const importCustomEventPolyfill = () =>
-  dynamic(() => import("custom-event-polyfill"), { ssr: false });
