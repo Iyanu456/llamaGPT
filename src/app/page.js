c95598206635e5d82bef230922b844cdfeff9635
iyanu456 <oyerindei13@gmail.com>
2023-10-16 01:29:04 +0100

    working on frontend

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
             </header>
             <div class="h-full px-2 py-5">example</div>
             <div class="grid place-items-center fixed top-[auto] bottom-0 right-0 left-16em input-container py-4">
-	  <form className="flex gap-3"><input><button>submit</button></form></div>
+	  <form className="flex gap-3"><input/><button>submit</button></form></div>
         </main>
 	  </div>
   )
