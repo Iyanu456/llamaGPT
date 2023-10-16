5b92aca3e6ebfdb09bd52c22027414b5ffd88930
iyanu456 <oyerindei13@gmail.com>
2023-10-16 02:07:19 +0100

    working on frontend

--- src/app/page.js
+++ src/app/page.js
@@ -8,10 +8,10 @@ export default function Home() {
     <div className="relative main-container w-[100%] h-screen">
         <aside></aside>
         <main>
-            <header className="max-sm:left-0 sticky top-0 right-0 left-[16em] bottom-[auto] px-3 py-[1.4em] grid place-items-center">
+            <header className="max-sm:left-0 fixed top-0 right-0 left-[16em] bottom-[auto] px-3 py-[1.4em] grid place-items-center">
                 <div>App Logo</div>
             </header>
-            <div class="h-[80%] px-2 py-[3em] chat-container">example</div>
+            <div class="h-[fit-content] max-h-[80%] px-2 py-[3.8em] chat-container">example</div>
             <div class="grid place-items-center fixed top-[auto] bottom-0 right-0 max-sm:left-0 left-[16em] input-container py-[1.6em]">
 	  <form className="flex gap-3"><input/><button>submit</button></form></div>
         </main>
