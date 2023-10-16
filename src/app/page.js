235d94adaff79d1f6aa4b9b736dc9a018dfccd49
iyanu456 <oyerindei13@gmail.com>
2023-10-16 03:22:08 +0100

    ui improvements

--- src/app/page.js
+++ src/app/page.js
@@ -8,13 +8,13 @@ export default function Home() {
   const [banner, setBanner] = useState(true);
 
   return (
-    <div className="relative main-container w-[100%] h-[100vh]">
+    <div className="relative main-container w-[100%] h-screen">
         <aside></aside>
         <main>
-            <header className="z-20 max-sm:left-0 fixed top-0 right-0 left-[16em] bottom-[auto] px-3 py-[1.4em] grid place-items-center">
+            <header className="px-3 py-[1.4em] grid place-items-center">
                 <div>App Logo</div>
             </header>
-            <div class="h-[70vh] z-0 chat-container">
+            <div class="chat-container">
 	  {banner && <div>
 		  <h2><b>LlamaGPT</b></h2>
 	  	<div className="example-container">
@@ -22,7 +22,7 @@ export default function Home() {
 	  	</div>
 	</div>}
 	  </div>
-            <div class="z-20 grid place-items-center fixed top-[auto] bottom-0 right-0 max-sm:left-0 left-[16em] input-container py-[1.6em]">
+            <div class="grid place-items-center input-container py-[1.6em]">
 	  <form className="flex gap-3" onSubmit={(e) => {
 		  e.preventDefault()
 		  !banner ? null : setBanner(false)
