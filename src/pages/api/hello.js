c0a40e95a724d0934ef0e7842d720827d16d5bd8
Iyanu456 <oyerindei13@gmail.com>
2023-10-20 23:24:56 +0100

    pulling

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,12 +1,5 @@
 // pages/api/hello.js
 
-import Cors from 'micro-cors';
-
-const cors = Cors({
-  origin: 'http://localhost:3001/api', // For testing purposes, allow requests from any origin
-  allowedMethods: ['POST'],
-});
-
 export default async function handler(req, res) {
   await cors(req, res);
 
@@ -14,10 +7,6 @@ export default async function handler(req, res) {
     try {
       // Handle the POST request here
       const data = req.body; // Assuming you expect JSON data
-
-      // Add the Access-Control-Allow-Origin header to allow cross-origin requests
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001/api'); // Update with the actual origin you want to allow
-
       // Process the data and send a response
       res.status(200).json({ message: 'POST request received', data });
     } catch (error) {
