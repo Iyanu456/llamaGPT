c50550122a32a8c5ca56bad698c12f70ea1ee2a2
Iyanu456 <oyerindei13@gmail.com>
2023-11-01 02:32:03 +0100

    modified llama2 model 2

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -18,7 +18,7 @@ export async function POST(req) {
     stream: true,
     // The model must support streaming. See https://replicate.com/docs/streaming
     // This is the model ID for Llama 2 70b Chat
-    version: 'f1726c9a8545e2cc051f54b55cd398351ccb145930f080d2eeea295f0d8b6abf',
+    version: 'de18b8b68ef78f4f52c87eb7e3a0244d18b45b3c67affef2d5055ddc9c2fb678',
     // Format the message list into the format expected by Llama 2
     // @see https://github.com/vercel/ai/blob/99cf16edf0a09405d15d3867f997c96a8da869c6/packages/core/prompts/huggingface.ts#L53C1-L78C2
     input: {
