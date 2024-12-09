99b36106640d54f109fd32b642b000b826016976
Iyanu <oyerindei13@gmail.com>
2024-12-09 16:21:05 +0100

    Update route.js

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -2,17 +2,6 @@ import { ReplicateStream, StreamingTextResponse } from 'ai';
 import Replicate from 'replicate';
 import { experimental_buildLlama2Prompt } from 'ai/prompts';
 
-export async function getStaticProps() {
-
- const replicate = new Replicate({
-  // Create a Replicate API client (that's edge friendly!)
-  auth: process.env.REPLICATE_API_KEY || '',
-});
-  // ...
-}
-
-console.log("Replicate API Key:", process.env.REPLICATE_API_KEY);
-
 
 
  
@@ -20,23 +9,23 @@ console.log("Replicate API Key:", process.env.REPLICATE_API_KEY);
 export const runtime = 'edge';
  
 export async function POST(req) {
+  console.log("Replicate API Key:", process.env.REPLICATE_API_KEY);
+  
+  const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_KEY || '',
+  });
+
   const { messages } = await req.json();
- 
+
   const response = await replicate.predictions.create({
-    // You must enable streaming.
     stream: true,
-    // The model must support streaming. See https://replicate.com/docs/streaming
-    // This is the model ID for Llama 2 70b Chat
     version: 'de18b8b68ef78f4f52c87eb7e3a0244d18b45b3c67affef2d5055ddc9c2fb678',
-    // Format the message list into the format expected by Llama 2
-    // @see https://github.com/vercel/ai/blob/99cf16edf0a09405d15d3867f997c96a8da869c6/packages/core/prompts/huggingface.ts#L53C1-L78C2
     input: {
       prompt: experimental_buildLlama2Prompt(messages),
     },
   });
- 
-  // Convert the response into a friendly text-stream
+
   const stream = await ReplicateStream(response);
-  // Respond with the stream
   return new StreamingTextResponse(stream);
 }
+
